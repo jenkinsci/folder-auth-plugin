9a15314f2002835b057a76d95449ba8df9e227fd
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2020-05-25 18:11:36 +0530

    Webpack generated React for configuration page

---
+++ src/main/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main/frontend/src',
+  output: {
+    path: path.resolve(__dirname, 'src/main/webapp/js'),
+    filename: 'folder-auth-bundle.js',
+  },
+  // enable source maps for debugging webpack output
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        loader: 'source-map-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx','.ts', '.tsx'],
+  }
+};
