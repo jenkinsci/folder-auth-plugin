b974e188ff6140b8a2d3dba60529915ec93def34
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2020-05-26 10:41:15 +0530

    Avoid collisions with JSON setup by prototype.js

---
+++ src/main/frontend/src/resetEnvironment.js
@@ -0,0 +1,14 @@
+/**
+ * Reset JS environment set up by prototype.js to not interfere with
+ * out react components.
+ *
+ * @see https://github.com/jenkinsci/jenkins/blob/75468da366c1d257a51655dcbe952d55b8aeeb9c/war/src/main/js/util/jenkins.js#L22
+ */
+export function resetEnvironment() {
+  if (Array.prototype.toJSON) {
+    delete Array.prototype.toJSON;
+    delete Object.prototype.toJSON;
+    delete Hash.prototype.toJSON;
+    delete String.prototype.toJSON;
+  }
+}
