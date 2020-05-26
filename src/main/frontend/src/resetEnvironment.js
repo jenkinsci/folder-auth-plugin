bb19ea8aa0a873282f3c9dc91f894bb9ad41b8a4
Abhyudaya Sharma <sharmaabhyudaya@gmail.com>
2020-05-26 14:09:45 +0530

    Update src/main/frontend/src/resetEnvironment.js
    Co-authored-by: Tim Jacomb <t.jacomb@kainos.com>

--- src/main/frontend/src/resetEnvironment.js
+++ src/main/frontend/src/resetEnvironment.js
@@ -1,6 +1,6 @@
 /**
  * Reset JS environment set up by prototype.js to not interfere with
- * out react components.
+ * our react components.
  *
  * @see https://github.com/jenkinsci/jenkins/blob/75468da366c1d257a51655dcbe952d55b8aeeb9c/war/src/main/js/util/jenkins.js#L22
  */
